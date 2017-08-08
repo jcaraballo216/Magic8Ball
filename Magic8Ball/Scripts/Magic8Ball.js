60ab086e8a26a96807f253e4a170ab41a7afa6df
Jerome <jscar216@gmail.com>
2017-08-08 15:15:05 -0400

    Added shake button

---
+++ Magic8Ball/Scripts/Magic8Ball.js
@@ -0,0 +1,9 @@
+﻿var fortune = ["Better Luck Next Time", "You're Going To Have A Good Day", "Your House Will Catch On Fire", "Love Is In The Air", "Nice Try"]
+
+function getRando() {
+    var num = Math.round(Math.random() * (fortune.length-1));
+    return num;
+}
+function getFortune() {
+    document.getElementById("fortune").innerHTML = fortune[getRando()]
+}
